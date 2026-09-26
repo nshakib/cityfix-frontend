5c7fe993321ea65c57ad3ea3af411988a99e57dd
nshakib <nazmus.shakib.cse@gmail.com>
2026-09-26 12:13:36 +0600

    feat: Registration page implement

---
+++ src/assets/svg/Logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+export default function Logo() {
+  return (
+    <Image
+      src=""
+      alt="Logo"
+      width={32}
+      height={32}
+      className="h-8 w-8"
+    />
+  );
+}
